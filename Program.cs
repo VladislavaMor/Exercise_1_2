e8434c31389b816d162970581e3bd06633dd37d6
Владислава <@LAPTOP-ER5FQP6F>
2024-01-24 20:31:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Hello ");
+            Write("World");
+            Write("!!!");
+            ReadLine();
+        }
+    }
+}
